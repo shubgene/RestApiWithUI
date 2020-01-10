fa531cbc5d869fc2c98db0cb7bceb421f62262a9
Shubham Rastogi <ershubham88@gmail.com>
2020-01-10 01:35:04 +0100

    added custom login page

--- assignment/src/main/java/com/asellion/assignment/assignment/Service/SecurityConfig.java
+++ assignment/src/main/java/com/asellion/assignment/assignment/Service/SecurityConfig.java
@@ -1,20 +1,21 @@
 package com.asellion.assignment.assignment.Service;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
  
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
         .csrf().disable()   
         .authorizeRequests()
-                .anyRequest().authenticated()
+        .anyRequest().authenticated()
                 .and()
-            //.formLogin().and()
-            .httpBasic();
- 
+            .formLogin().loginPage("/login").permitAll();
     }
 }
